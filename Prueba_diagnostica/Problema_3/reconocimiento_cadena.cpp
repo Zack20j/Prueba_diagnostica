247f8f165aab91f1c26d5a81d726eea760fdc1dd
Zack20j <joeydaniels291202@gmail.com>
2025-11-01 17:40:03 -0400

    problema 3 hecho

---
+++ Prueba_diagnostica/Problema_3/reconocimiento_cadena.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+int main() {
+    std::string cadena;
+    std::cout << "Ingrese una cadena: ";
+    std::getline(std::cin, cadena);
+
+    std::regex regex_scientific(R"(^[+-]?(\d+(\.\d*)?|\.\d+)[eE][+-]?\d+$)");
+    std::regex regex_ip(R"(^((25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$)");
+    std::regex regex_email(R"(^[\w\.-]+@[\w\.-]+\.\w{2,}$)");
+
+    if (std::regex_match(cadena, regex_scientific))
+        std::cout << "Notación cientifica\n";
+    else if (std::regex_match(cadena, regex_ip))
+        std::cout << "Direccion IP\n";
+    else if (std::regex_match(cadena, regex_email))
+        std::cout << "Correo electronico\n";
+    else
+        std::cout << "No reconocido\n";
+
+    return 0;
+}
